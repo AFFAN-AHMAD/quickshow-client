c1906dd71f2c6adc17d403a2eee16637ee74f357
affan <affan.ahmad@superprocure.com>
2025-06-27 10:19:19 +0530

    27 jun: wired the api endpoint for dashboard data

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -10,8 +10,12 @@ import Loading from "../../components/Loading";
 import Title from "../../components/admin/Title";
 import BlurCircle from "../../components/BlurCircle";
 import { dateFormat } from "../../lib/dateFormat";
+import { dummyDashboardData } from "../../assets/assets";
+import toast from "react-hot-toast";
+import { useAppContext } from "../../context/AppContext";
 
 const Dashboard = () => {
+  const { axios, getToken, user, image_base_url } = useAppContext();
   const currency = import.meta.env.VITE_CURRENCY;
   const [dashboardData, setDashboardData] = useState({
     totalBookings: 0,
@@ -43,13 +47,31 @@ const Dashboard = () => {
     },
   ];
   const fetchDashBoardData = async () => {
-    setDashboardData();
-    setLoading(false);
+    // setDashboardData(dummyDashboardData);
+    // setLoading(false);
+    try {
+      const { data } = await axios.get("/api/admin/dashboard", {
+        headers: {
+          Authorization: `Bearer ${await getToken()}`,
+        },
+      });
+      if (data.success) {
+        setDashboardData(data.dashboardData);
+        setLoading(false);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (err) {
+      console.log(err);
+      toast.error("Error fetching dashboard data", err);
+    }
   };
 
   useEffect(() => {
-    fetchDashBoardData();
-  }, []);
+    if (user) {
+      fetchDashBoardData();
+    }
+  }, [user]);
   return !loading ? (
     <>
       <Title text1={"Admin"} text2={"Dashboard"} />
@@ -79,7 +101,7 @@ const Dashboard = () => {
             className="w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20 hover:-translate-y-1 transition duration-300"
           >
             <img
-              src={show.movie.poster_path}
+              src={image_base_url + show.movie.poster_path}
               alt=""
               className="h-60 w-full object-cover"
             />
